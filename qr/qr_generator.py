5213df4d714a8719a9231aba27fa3d38f497e7aa
Skablam <stainlesssteelmatt@gmail.com>
2014-11-06 16:03:41 +0000

    changed qr url

--- qr/qr_generator.py
+++ qr/qr_generator.py
@@ -1,6 +1,6 @@
 import pyqrcode, sys
 
 def generate_qr_code(code):
-    url = pyqrcode.create('http://prove-it.gov.uk/checkreference/%s' % code)
+    url = pyqrcode.create('http://lr-prove-it.herokuapp.com/checkreference/%s' % code)
     url.svg('static/prove-it.svg', scale=4, module_color="#7D007D")
     return True
