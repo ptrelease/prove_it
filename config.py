80e4546141c1feffdf5840ab7e62461c05842e6d
Skablam <stainlesssteelmatt@gmail.com>
2014-11-06 16:00:32 +0000

    added config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+ACCOUNT_SID = os.environ.get("acc_sid")
+AUTH_TOKEN = os.environ.get("auth_token")
+TO_TEST_NUMBER = os.environ.get("to_number")
+FROM_TEST_NUMBER = os.environ.get("from_number")
