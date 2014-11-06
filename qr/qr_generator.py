364251d8ca0def01fa0a2daab4b4f12e094799e0
tsangsimon <simon.tsang@gmail.com>
2014-11-06 16:18:51 +0000

    refresh qr file

--- qr/qr_generator.py
+++ qr/qr_generator.py
@@ -1,6 +1,11 @@
 import pyqrcode, sys
+import os.path
 
 def generate_qr_code(code):
+
+    if os.path.exists("/static/prove-it.svg"):
+        os.remove("/static/prove-it.svg")
+
     url = pyqrcode.create('http://lr-prove-it.herokuapp.com/checkreference/%s' % code)
     url.svg('static/prove-it.svg', scale=4, module_color="#7D007D")
     return True
