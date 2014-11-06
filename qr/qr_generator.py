a034e59346f3b99d990f6c1b2e59132023350eee
Skablam <stainlesssteelmatt@gmail.com>
2014-11-06 16:49:57 +0000

    changed qr config

--- qr/qr_generator.py
+++ qr/qr_generator.py
@@ -6,6 +6,6 @@ def generate_qr_code(code):
     if os.path.exists("/static/prove-it.svg"):
         os.remove("/static/prove-it.svg")
 
-    url = pyqrcode.create('http://lr-prove-it.herokuapp.com/checkreference/%s' % code)
+    url = pyqrcode.create('"http://lr-prove-it.herokuapp.com/checkreference/%s"' % code)
     url.svg('static/prove-it.svg', scale=4, module_color="#7D007D")
     return True
