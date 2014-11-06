20875ae5a09ceb886829ba849313c7d455e005d7
treacle74 <ptrelease@gmail.com>
2014-11-06 11:08:16 +0000

    added route to property serach

--- views.py
+++ views.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route('/')
 def index():
   return render_template("index.html")
+
+@app.route('/choose')
+def choose():
+    return render_template("choose_to_prove.html")
+
+@app.route('/searchforproperty')
+def searchforproperty():
+    return render_template('search_for_property.html')
